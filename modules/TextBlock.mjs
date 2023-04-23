37a1cd18f453cdb287969f151def9ef8c821c411
ReachBlueDot <128635131+ReachBlueDot@users.noreply.github.com>
2023-04-22 20:15:28 -0400

    download

--- modules/TextBlock.mjs
+++ modules/TextBlock.mjs
@@ -323,7 +323,7 @@ function textMeasure(element, textBlock) {
 /*     let divWidth = noShowDiv.clientWidth + "px"; */
 
     //TEST - none
-    //noShowDiv.style.display = "none";
+    noShowDiv.style.display = "none";
 
     let headerPgs = headerSpace(textBlock.pagePerSectionHead, textBlock.numSections);
     let addInPages = (2 * textBlock.flyLeaves) + textBlock.titlePage + textBlock.tableOfContentsPage + textBlock.infoPage + headerPgs + textBlock.imagePages;
